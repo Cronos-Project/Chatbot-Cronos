68c79792f964190ea78052a66168a49ecf11b972
alex518123 <alexresende675@gmail.com>
2025-09-17 16:46:17 -0300

    Update server.js
    Apagando comentários desnecessários pós testes

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Servidor rodando junto com o bot 🚀');
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Servidor Express ativo na porta ${PORT}`);
-// });
-
 const express = require('express');
 const cors = require('cors');
 const conectarMongo = require('./db');
